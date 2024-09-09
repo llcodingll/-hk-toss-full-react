aa7ea52acbc756587bfbcba3c686165468a79e04
tjdanqkr <tjdanqkr@gmail.com>
2024-09-09 15:23:25 +0900

    close #6

--- src/Test2.jsx
+++ src/Test2.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 const Test2 = ()=>{
+    const [txt, setTxt] = useState("")
     return <div>
-        <input name="text" />
+        <input name="text" onChange={(e)=> setTxt(e.target.value)} />
     </div>
 }
 
