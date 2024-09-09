e0621dc1971acaa39cf8ae2f8afe5cbdb3a31661
tjdanqkr <tjdanqkr@gmail.com>
2024-09-09 15:27:51 +0900

    close #7

--- src/Test2.jsx
+++ src/Test2.jsx
@@ -2,8 +2,14 @@ import { useState } from "react";
 
 const Test2 = ()=>{
     const [txt, setTxt] = useState("")
+    const [logs, setLogs] = useState([])
+    const onChangeTxt = (e) => {
+        setTxt(e.target.value)
+        setLogs([...logs, e.target.value])
+    }
     return <div>
-        <input name="text" onChange={(e)=> setTxt(e.target.value)} />
+        <input name="text" onChange={onChangeTxt} />
+        {logs.map(str => <p>{str}</p>)}
     </div>
 }
 
